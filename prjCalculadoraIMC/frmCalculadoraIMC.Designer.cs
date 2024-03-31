23ce123d221af4648e9b711db2d0a782d8b7a369
Jermeson <jemersonferreira747@gmail.com>
2024-03-31 16:09:14 -0300

    Adicionar arquivos de projeto.

---
+++ prjCalculadoraIMC/frmCalculadoraIMC.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace prjCalculadoraIMC
+{
+    partial class frmCalculadoraIMC
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(14, 43);
+            this.txtAltura.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(160, 38);
+            this.txtAltura.TabIndex = 0;
+            this.txtAltura.Tag = "";
+            this.txtAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(14, 164);
+            this.txtPeso.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(160, 38);
+            this.txtPeso.TabIndex = 1;
+            this.txtPeso.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(14, 249);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(160, 70);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Altura*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Peso*";
+            // 
+            // frmCalculadoraIMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(201, 334);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "frmCalculadoraIMC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ prjCalculadoraIMC/frmCalculadoraIMC.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjCalculadoraIMC
+{
+    public partial class frmCalculadoraIMC : Form
+    {
+
+        
+
+        public frmCalculadoraIMC()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Double altura = Convert.ToDouble(txtAltura.Text.Replace(".", ","));
+                Double peso = Convert.ToDouble(txtPeso.Text.Replace(".", ","));
+
+                Double soma = peso / (altura * altura);
+
+
+                if (soma < 16)
+                {
+                    MessageBox.Show("IMC = " + soma.ToString("F") + " Baixo peso (Grau 1)");
+                }
+                else if (soma >= 16 && soma < 17)
+                {
+                    MessageBox.Show("IMC = " + soma.ToString("F") + " Baixo peso (Grau 2)");
+                }
+                else if (soma >= 17 && soma < 18.50)
+                {
+                    MessageBox.Show("IMC = " + soma.ToString("F") + " Baixo peso (Grau 3)");
+                }
+                else if (soma >= 18.50 && soma < 25)
+                {
+                    MessageBox.Show("IMC = " + soma.ToString("F") + " Peso adequado");
+                }
+                else if (soma >= 25 && soma < 30)
+                {
+                    MessageBox.Show("IMC = " + soma.ToString("F") + " Sobrepeso");
+                }
+                else if (soma >= 30 && soma < 35)
+                {
+                    MessageBox.Show("IMC = " + soma.ToString("F") + " Obesidade (Grau 1)");
+                }
+                else if (soma >= 35 && soma < 40)
+                {
+                    MessageBox.Show("IMC = " + soma.ToString("F") + " Obesidade (Grau 2)");
+                }
+                else if (soma >= 40)
+                {
+                    MessageBox.Show("IMC = " + soma.ToString("F") + " Obesidade (Grau 3)");
+                }
+            }
+            catch(Exception)
+            {          
+                MessageBox.Show("Prencha os campos corretamente");
+            }
+        }
+    }
+}
